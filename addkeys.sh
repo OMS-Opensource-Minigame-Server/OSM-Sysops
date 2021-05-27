00a9d73dd83e10b57ecbf35a7c1770acf869c1b6
funniray <funniray10@gmail.com>
2021-05-27 17:39:21 +0100

    Update addkeys.sh

--- addkeys.sh
+++ addkeys.sh
@@ -13,18 +13,18 @@ fi
 #Download the new keys from github
 for username in $usernames
 do
-  curl -f https://github.com/"$username".keys >> ~/.ssh/new_authorized_keys
+  curl -f https://github.com/"$username".keys >> /home/administrator/.ssh/new_authorized_keys
   status=$?
   if [ $status -ne 0 ] 
   then
     echo "Error downloading public keys from GitHub! Aborting."
-    rm ~/.ssh/new_authorized_keys
+    rm /home/administrator/.ssh/new_authorized_keys
     exit $status
   fi
 done
 
 #Remove the current authorized_keys file
-rm ~/.ssh/authorized_keys
+rm /home/administrator/.ssh/authorized_keys
 
 #Replace the current authorized_keys file with the new authorized keys.
-mv ~/.ssh/new_authorized_keys ~/.ssh/authorized_keys
+mv /home/administrator/.ssh/new_authorized_keys /home/administrator/.ssh/authorized_keys
