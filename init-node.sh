a45d0e75a3eaa639ce5a70a832b6e378d2af3b77
SilentBot1 <silentbot1@gmail.com>
2021-05-30 15:34:16 +0100

    Update init-node.sh script. - Switches from using just Kubernetes to using Microk8s to manage Kubernetes.

--- init-node.sh
+++ init-node.sh
@@ -19,38 +19,27 @@ chmod +x /etc/cron.daily/addkeys.sh
 echo "Allowing admin group to run sudo passwordless"
 echo "administrator ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-echo "Installing Docker prerequisites"
+echo "Installing Useful Utilities"
 sudo apt-get update
 sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-echo "Installing Docker 18.0.9"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install -y docker-ce=5:18.09.9~3-0~ubuntu-bionic docker-ce-cli=5:18.09.9~3-0~ubuntu-bionic
-sudo apt-mark hold docker-ce docker-ce-cli
+echo "Installing Microk8s"
+sudo snap install microk8s --classic --channel=1.19/stable
+sudo ufw allow in on cni0 && sudo ufw allow out on cni
+sudo ufw default allow routed
 
-echo "Setting Kubernetes prerequisites"
-sudo modprobe br_netfilter
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sudo sysctl --system
+sudo usermod -a -G microk8s administrator
+sudo chown -f -R administrator ~/.kube
+sudo snap alias microk8s.kubectl kubectl
 
-echo "Installing Kubernetes"
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet=1.18.19-00 kubeadm=1.18.19-00 kubectl=1.18.19-00
-sudo apt-mark hold kubelet kubeadm kubectl
 
-echo "Joining node to the cluster"
-read -p "Enter the Kubernetes master hostname: " khostname
-read -p "Enter the Kubernetes token: " ktoken
-read -p "Enter the Kubernetes discovery-token-ca-cert-hash: " kcerthash
+read -r -n 1 -t 10 -p "If you would like to manually join the Microk8s cluster, please press any key!" mk8sjoin
+EXIT_STATUS=$?
+if [ $EXIT_STATUS -eq 0 ]; then
 
-kubeadm join "$khostname:6443" --token "$ktoken" --discovery-token-ca-cert-hash "sha256:$kcerthash"
+  echo "Joining node to the cluster"
+  read -r -p "Enter the Microk8s master hostname and port, e.g. 10.100.100.18:25000: " mk8shostname
+  read -r -p "Enter the Microk8s join token: " mk8stoken
+
+  sudo microk8s join "$mk8shostname/$mk8stoken"
+fi
